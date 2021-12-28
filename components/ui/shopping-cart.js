0c719cbe65caa26d335fbdcb2bddaf1939b96faa
Dylan Travis <dylan.travis23@outlook.com>
2021-12-27 19:48:11 -0800

    shopping-cart state set to false when clicked outside content

--- components/ui/shopping-cart.js
+++ components/ui/shopping-cart.js
@@ -1,10 +1,26 @@
+import { useRef, useEffect } from "react";
 import classes from "./shopping-cart.module.css";
 import { BsX } from "react-icons/bs";
+
 function ShoppingCart(props) {
   const { setActive } = props;
+  const modalRef = useRef(null);
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setActive(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [modalRef]);
+
   return (
-    <section className={classes.container}>
-      <div className={classes.content}>
+    <section id="container" className={classes.container}>
+      <div ref={modalRef} className={classes.content}>
         <div className={classes.header}>
           <h4>Shopping Cart</h4>
           <BsX className={classes.icon} onClick={() => setActive(false)} />
