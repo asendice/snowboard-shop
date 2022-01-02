cdc2f501cb52c200ce1bae594c35d273210636ed
Dylan Travis <dylan.travis23@outlook.com>
2022-01-01 20:34:48 -0800

    rmvd console.log and dummy data

--- components/home-page/featured.js
+++ components/home-page/featured.js
@@ -1,55 +1,15 @@
 import classes from "./featured.module.css";
 import ProductSlider from "./product-slider";
 
-const DUMMY_DATA = [
-  {
-    id: "01",
-    title: "Snowboard Rosi",
-    img: "/images/test-featured.jpeg",
-    price: 499.99,
-  },
-  {
-    id: "02",
-    title: "Burton Snowboard",
-    img: "/images/snowboard-header.webp",
-    price: 499.99,
-  },
-  {
-    id: "03",
-    title: "Snowboard Rosi",
-    img: "/images/test-featured.jpeg",
-    price: 499.99,
-  },
-  {
-    id: "04",
-    title: "Snowboard Rosi",
-    img: "/images/test-featured.jpeg",
-    price: 499.99,
-  },
-  {
-    id: "05",
-    title: "Snowboard Rosi",
-    img: "/images/test-featured.jpeg",
-    price: 499.99,
-  },
-  {
-    id: "06",
-    title: "Snowboard Rosi",
-    img: "/images/test-featured.jpeg",
-    price: 499.99,
-  },
-]
-
 function Featured(props) {
   const { snowboards, clothing } = props;
-  console.log(clothing, "clothing")
   return (
     <div className={classes.container}>
       <h2>Featured</h2>
       <h3>Snowboards</h3>
       <ProductSlider items={snowboards} category={'snowboards'}/>
       <h3>Clothes</h3>
-      <ProductSlider items={clothing} />
+      <ProductSlider items={clothing} category={'clothes'} />
     </div>
   )
 }
