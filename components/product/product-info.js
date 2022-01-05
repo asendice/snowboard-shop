b25de48869bdc7dcd5b3902bd8c47147cb654297
Dylan Travis <dylan.travis23@outlook.com>
2022-01-05 15:34:58 -0800

    cart includes item rendertext instead of add to cart btn

--- components/product/product-info.js
+++ components/product/product-info.js
@@ -1,24 +1,23 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import classes from "./product-info.module.css";
+import Link from "next/link";
 import { BsCart3 } from "react-icons/bs";
 import { useCart, useCartUpdate } from "../../store/cart-context";
 function ProductInfo(props) {
   const { title, price, size, images, _id } = props.product;
   const [selectedSize, setSelectedSize] = useState(size[0]);
-  const quantityRef = useRef("1");
+  const [qty, setQty] = useState(1);
   const cart = useCart();
   const updateCart = useCartUpdate();
 
   function addToCartHandler() {
-    const enteredQuantity = quantityRef.current.value;
-
     const product = {
       _id: _id,
       title: title,
       price: price,
       size: selectedSize,
       images: images,
-      qty: enteredQuantity,
+      qty: qty,
     };
 
     updateCart("add", product);
@@ -35,18 +34,46 @@ function ProductInfo(props) {
       <h5>Select a size: </h5>
       <ul className={classes.size}>
         {props.category === "snowboards" &&
-          size.map((item) => <li onClick={() => setSelectedSize(item)} key={item}>{item}cm</li>)}
+          size.map((item) => (
+            <li
+              key={item}
+              className={selectedSize === item && classes.selected}
+              onClick={() => setSelectedSize(item)}
+            >
+              {item}cm
+            </li>
+          ))}
         {props.category === "clothes" &&
-          size.map((item) => <li onClick={() => setSelectedSize(item)} key={item}>{item}</li>)}
+          size.map((item) => (
+            <li
+              key={item}
+              className={selectedSize === item && classes.selected}
+              onClick={() => setSelectedSize(item)}
+            >
+              {item}
+            </li>
+          ))}
       </ul>
       <div className={classes.qty}>
         <h5>Qty: </h5>
-        <input ref={quantityRef} type="number" min="1" max="9" />
+        <input
+          onChange={(e) => setQty(e.target.value)}
+          value={qty}
+          type="number"
+          min="1"
+          max="9"
+        />
       </div>
-      <a className={classes.addToCart} onClick={() => addToCartHandler()}>
-        <h5>Add To Cart</h5>
-        <BsCart3 className={classes.cartIcon} />
-      </a>
+      {!cart.map((item) => item._id === _id).includes(true) ? (
+        <a className={classes.addToCart} onClick={() => addToCartHandler()}>
+          <h5>Add To Cart</h5>
+          <BsCart3 className={classes.cartIcon} />
+        </a>
+      ) : (
+        <h5>
+          Item Added To Cart <Link href="/products"><a className={classes.continue}>Continue Shopping</a></Link>
+        </h5>
+      )}
     </div>
   );
 }
