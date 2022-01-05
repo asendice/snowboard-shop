122879b55570bfefccf2d8df48498e49408fe1be
Dylan Travis <dylan.travis23@outlook.com>
2022-01-05 06:32:15 -0800

    filterlist add text

--- components/products-page/filter-list.js
+++ components/products-page/filter-list.js
@@ -4,12 +4,13 @@ function FilterList(props) {
   const options = props.filters.map((item) => item.option).reverse();
   return (
     <Fragment>
+    <p>Filters:</p>
       <ul className={classes.filters}>
         {options.map((item) => (
           <li key={item}>{`${item},`} </li>
         ))}
+        <a className={classes.clear} onClick={() => props.setFilters([])}>clear</a>
       </ul>
-      <a className={classes.clear} onClick={() => props.setFilters([])}>clear</a>
     </Fragment>
   );
 }
