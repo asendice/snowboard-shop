0bbeb3d3bf8aee7585d9ec5e06731463529b8fcf
Dylan Travis <dylan.travis23@outlook.com>
2022-01-16 11:05:59 -0800

    postEmail api fetch function

--- helpers/api-utils.js
+++ helpers/api-utils.js
@@ -9,8 +9,8 @@ export async function getFeaturedSnowboards() {
 }
 
 export async function getSnowboard(title) {
-  const boards = await  getAllSnowboards();
-  return boards.filter((board) => board.title === title)
+  const boards = await getAllSnowboards();
+  return boards.filter((board) => board.title === title);
 }
 export async function getAllClothes() {
   const response = await fetch("http://localhost:3000/api/clothes");
@@ -23,12 +23,24 @@ export async function getFeaturedClothing() {
 }
 
 export async function getClothingItem(title) {
-  const clothes = await  getAllClothes();
-  return clothes.filter((item) => item.title === title)
+  const clothes = await getAllClothes();
+  return clothes.filter((item) => item.title === title);
 }
 
 export async function getAllProducts() {
   const boards = await getAllSnowboards();
   const clothes = await getAllClothes();
   return boards.concat(clothes).sort().flat();
+}
+
+export async function postEmail(email) {
+  const postObj = JSON.stringify({ email: email });
+  const request = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: postObj,
+  };
+  const response = await fetch("http://localhost:3000/api/email-list", request);
+  const data = await response.json();
+  return data;
 }
