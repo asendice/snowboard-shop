18d4523bf0b121c3bd3c5daaeaa3a495f303693f
Dylan Travis <dylan.travis23@outlook.com>
2022-01-19 12:02:36 -0800

    added savings modal to homepage

--- components/home-page/featured.js
+++ components/home-page/featured.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 import classes from "./featured.module.css";
 import ProductSlider from "./product-slider";
+import SavingsModal from "../savings/savings-modal";
 
 function Featured(props) {
+  const [scrollY, setScrollY] = useState(0);
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  useEffect(() => {
+    if (scrollY >= 1400){
+      setActive(true)
+    }
+  })
   const { snowboards, clothing } = props;
   return (
     <div className={classes.container}>
@@ -10,6 +31,7 @@ function Featured(props) {
       <ProductSlider items={snowboards} category={'snowboards'}/>
       <h3>Clothes</h3>
       <ProductSlider items={clothing} category={'clothes'} />
+      {active === true && <SavingsModal setActive={setActive} active={active}/> }
     </div>
   )
 }
