9a2e9f4ac74a54c8e6aab17ec4a60729efb89871
Dylan Travis <dylan.travis23@outlook.com>
2022-01-21 15:21:28 -0800

    client side email validation and ui

--- components/savings/savings-modal.js
+++ components/savings/savings-modal.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import classes from "./savings-modal.module.css";
 import { BsX } from "react-icons/bs";
 import { postEmail } from "../../helpers/api-utils";
@@ -6,13 +6,12 @@ import { useSave } from "../../store/save-context";
 import { useNotification } from "../../store/notification-context";
 
 function SavingsModal(props) {
+  const [inputError, setInputError] = useState("");
   const modalRef = useRef();
   const emailRef = useRef();
 
   const save = useSave();
   const notification = useNotification();
-
- 
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -27,30 +26,37 @@ function SavingsModal(props) {
     };
   }, [modalRef]);
 
-  function activateOfferHandler() {
+  function emailValidation() {
     const email = emailRef.current.value;
-    if (email.length > 0) {
-      postEmail(emailRef.current.value)
-        .then((response) => {
-          if(response.status === 200){
-            props.setActive(false);
-            save.updateEmail(response.email.email);
-            notification.showNotification({
-              title: `${response.email.email}`,
-              message: `Accepted! 15% automatically added at checkout`,
-              status: "success",
-            });
-          }
-          if(response.status !== 200){
-            notification.showNotification({
-              title: `FAILED: `,
-              message: `${response.error}`,
-              status: "failed",
-            });
-          }
-        })
-
+    const regex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (email.length === 0 || regex.test(email) === false) {
+      setInputError("Invalid Email");
+    } else {
+      activateOfferHandler(email);
     }
+  }
+
+  function activateOfferHandler(text) {
+    setInputError("");
+    postEmail(text).then((response) => {
+      if (response.status === 200) {
+        props.setActive(false);
+        save.updateEmail(response.email.email);
+        notification.showNotification({
+          title: `${response.email.email}`,
+          message: `Accepted! 15% automatically added at checkout`,
+          status: "success",
+        });
+      }
+      if (response.status !== 200) {
+        notification.showNotification({
+          title: `FAILED: `,
+          message: `${response.error}`,
+          status: "failed",
+        });
+      }
+    });
   }
 
   return (
@@ -68,12 +74,17 @@ function SavingsModal(props) {
         <h4>Gear Up!</h4>
         <h3>GET 15%! OFF*</h3>
         <p className={classes.subText}>your first order when you sign up!</p>
-        <input
-          ref={emailRef}
-          className={classes.input}
-          placeholder="ENTER EMAIL ADDRESS"
-        />
-        <a className={classes.btn} onClick={() => activateOfferHandler()}>
+        <div>
+          <input
+            ref={emailRef}
+            className={classes.input}
+            placeholder="ENTER EMAIL ADDRESS"
+          />
+          {inputError.length > 0 && (
+            <p className={classes.error}>{inputError}*</p>
+          )}
+        </div>
+        <a className={classes.btn} onClick={() => emailValidation()}>
           ACTIVATE OFFER
         </a>
         <a className={classes.cancel} onClick={() => props.setActive(false)}>
