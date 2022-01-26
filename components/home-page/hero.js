794b997b7a0608a7529ebfa9038a80f0cc80d9db
Dylan Travis <dylan.travis23@outlook.com>
2022-01-26 13:04:43 -0800

    hero comp mobile friendlyy

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,12 +1,13 @@
 import CtaButton from "../ui/cta-button";
 import classes from "./hero.module.css";
 
-
 function Hero() {
   return (
     <div className={classes.container}>
       <h1>Shred down the mountains with the best gear</h1>
+      <div className={classes.btn}>
         <CtaButton link={"/products"}> Shop The Best </CtaButton>
+      </div>
     </div>
   );
 }
